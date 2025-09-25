22bae04e6ba69f7f66d9bd60a0638167b2dfc1e8
Jarius School <Jbedward1640@conestogac.on.ca>
2025-09-25 18:56:05 -0400

    Fully implemented DataPreperation.py

--- DataPreperation/DataPreperation.py
+++ DataPreperation/DataPreperation.py
@@ -1,17 +1,30 @@
-class DataPreperation:
-    def __init__(self, data):
-        self.data = data
-        self.prepared_data = None
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
 
-    def clean_data(self):
-        # TODO: implement data cleaning login (dropping NaN, potentially removing 0's)
-        pass
+class DataPreparation:
+    def __init__(self, df: pd.DataFrame):
+        self.df = df.copy()
+        self.axis_cols = [col for col in df.columns if col.startswith("Axis #")]
+        self.df_std = None
+        self.df_norm = None
 
-    def transform_data(self):
-        # TODO implement scaling/normalization or standardization (MinmaxScaler, StandardScaler)
-        pass
+    def clean_data(self) -> pd.DataFrame:
+        self.df.fillna(0, inplace=True)
+        self.df = self.df[(self.df[self.axis_cols] != 0).any(axis=1)]
+        return self.df
 
-    def get_prepared_data(self):
-        # TODO: call clean + transform, then return
-        pass
+
+    def normalize(self) -> pd.DataFrame:
+        scaler = MinMaxScaler
+        self.df_norm = self.df.copy()
+        self.df_norm[self.axis_cols] = scaler.fit_transform(self.df[self.axis_cols])
+        return self.df_norm
+
+    def standardize(self) -> pd.DataFrame:
+
+       scaler = StandardScaler()
+       self.df_std = self.df.copy()
+       self.df_std[self.axis_cols] = scaler.fit_transform(self.df[self.axis_cols])
+       self.df_std["time_min"] = (self.df_std["Time"] - self.df_std["Time"].min()).dt.total_seconds() / 60
+       return self.df_std
 
